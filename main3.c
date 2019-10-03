aa4daa710438d621713134b270148d37d8396d26
1915157 <chonsa0409@naver.com>
2019-10-03 20:44:25 +0900

    op04_1_2_3
    all of op04 (1,2,3)

--- main3.c
+++ main3.c
@@ -4,6 +4,39 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+// op04_01
+	
+	int x,y,z,m;
+	int a,b,c;
+	
+	x=1;
+	a=3;
+	b=4;
+	c=5;
+	
+	y = m*x+b;
+	y = a*x*x + b*x + c;
+	m = (x+y+z)/3;
+	
+	printf("y=%d, m=%d", y,m);
+
+
+//04op_02
+
+	int x = 3;
+	int cond1, cond2;
+	
+	cond1 = 2 < x && x < 5;
+	cond2 = x>1 || x<4 && x>3;
+	
+	printf("cond1 = %d, cond2 = %d", cond1, cond2);
+	
+	return 0;
+
+
+//04op_03
+
 	int x;
 	int y;
 	
@@ -17,4 +50,6 @@ int main(int argc, char *argv[]) {
 	printf(" %% result is : %d\n", x%y);
 	
 	return 0;
+
 }
+
